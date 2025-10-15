91811c4783bbebe3c797b8aa5a64f7ae1fbd3d23
Nauman <naumansqb@gmail.com>
2025-10-15 15:20:53 -0400

    Removed setters as no use case for them

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -17,49 +17,24 @@ public class Transaction{
         this.vendor = vendor;
         this.amount = amount;
     }
-
     public LocalTime getTime() {
         return time;
     }
-
     public LocalDate getDate() {
         return date;
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
-    }
-
-    public void setTime(LocalTime time) {
-        this.time = time;
-    }
-
-
-
     public String getDescription() {
         return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public String getVendor() {
         return vendor;
     }
 
-    public void setVendor(String vendor) {
-        this.vendor = vendor;
-    }
-
     public double getAmount() {
         return amount;
     }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
-
 
     @Override
     public String toString() {
